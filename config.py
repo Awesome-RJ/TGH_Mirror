617e131b584d5456194435653b1a0a4b82db6e00
Dot <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-12 10:56:33 +0530

    Update config.py
    Added Fresh BOT TOKEN

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # REQUIRED CONFIG
-BOT_TOKEN = "7713486921:AAF4akLKvq-XUZnJg6DBy-nFeRn0GHwa3Hs" # Enter your telegram bot token
+BOT_TOKEN = "1843295508:AAHOpB4oCqk44i2NxArC3Ln7FPZTe3z6dMk" # Enter your telegram bot token
 OWNER_ID = 2076764009  # Enter your telegram user id
 TELEGRAM_API = 3975570 # Enter telegram API key
 TELEGRAM_HASH = "680b62f2844aa1954216f6cb99d2f3d9" # Enter telegram API hash
