f3a81c5983a08c1cbd85e0f8da1f030f858dee43
Dot <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-15 21:43:44 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="tghbot",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies are already in requirements.txt
+    ],
+)
