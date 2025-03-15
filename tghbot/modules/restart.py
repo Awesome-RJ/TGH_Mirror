0c21e23380341965ca15acdc89c9fa141d1dd56d
Dot <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-15 21:04:45 +0530

    Update restart.py

--- tghbot/modules/restart.py
+++ tghbot/modules/restart.py
@@ -21,57 +21,21 @@ from tghbot.helper.telegram_helper.message_utils import delete_message, send_mes
 
 @new_task
 async def restart_bot(_, message):
-async def restart(_, message):
-    intervals["stopAll"] = True
-    restart_message = await send_message(
-        message,
-        "Restarting..."
-    )
-    if scheduler.running:
-        scheduler.shutdown(wait=False)
-    if qb := intervals["qb"]:
-        qb.cancel()
-    if jd := intervals["jd"]:
-        jd.cancel()
-    if nzb := intervals["nzb"]:
-        nzb.cancel()
-    if st := intervals["status"]:
-        for intvl in list(st.values()):
-            intvl.cancel()
-    await sync_to_async(clean_all)
-    if sabnzbd_client.LOGGED_IN:
-        await gather(
-            sabnzbd_client.pause_all(),
-            sabnzbd_client.purge_all(True),
-            sabnzbd_client.delete_history(
-                "all",
-                delete_files=True
-            ),
-        )
-    proc1 = await create_subprocess_exec(
-        "pkill",
-        "-9",
-        "-f",
-        "gunicorn|aria2c|qbittorrent-nox|ffmpeg|rclone|java|sabnzbdplus"
-    )
-    proc2 = await create_subprocess_exec(
-        "python3",
-        "update.py"
-    )
-    await gather(
-        proc1.wait(),
-        proc2.wait()
-    )
-    async with aiopen(
-        ".restartmsg",
-        "w"
-    ) as f:
-        await f.write(f"{restart_message.chat.id}\n{restart_message.id}\n") # type: ignore
-    osexecl(
-        executable,
-        executable,
-        "-m",
-        "bot"
+    buttons = button_build.ButtonMaker()
+    buttons.data_button("Yes!", "botrestart confirm")
+    buttons.data_button("Cancel", "botrestart cancel")
+    button = buttons.build_menu(2)
+    await send_message(message, "Are you sure you want to restart the bot ?!", button)
+
+
+@new_task
+async def restart_sessions(_, message):
+    buttons = button_build.ButtonMaker()
+    buttons.data_button("Yes!", "sessionrestart confirm")
+    buttons.data_button("Cancel", "sessionrestart cancel")
+    button = buttons.build_menu(2)
+    await send_message(
+        message, "Are you sure you want to restart the session(s) ?!", button
     )
 
 
@@ -98,36 +62,32 @@ async def send_incomplete_task_message(cid, msg_id, msg):
 
 async def restart_notification():
     if await aiopath.isfile(".restartmsg"):
-        async with aiopen(".restartmsg") as f:
-            content = await f.read()
-            chat_id, msg_id = map(int, content.splitlines())
+        with open(".restartmsg") as f:
+            chat_id, msg_id = map(int, f)
     else:
         chat_id, msg_id = 0, 0
 
-    if (
-        Config.INCOMPLETE_TASK_NOTIFIER
-        and Config.DATABASE_URL
-        and (notifier_dict := await database.get_incomplete_tasks())
-    ):
-        for cid, data in notifier_dict.items():
-            msg = "Restarted Successfully!" if cid == chat_id else "Bot Restarted!"
-            for tag, links in data.items():
-                msg += f"\n\n{tag}: "
-                for index, link in enumerate(links, start=1):
-                    msg += f" <a href='{link}'>{index}</a> |"
-                    if len(msg.encode()) > 4000:
-                        await send_incomplete_task_message(cid, msg_id, msg)
-                        msg = ""
-            if msg:
-                await send_incomplete_task_message(cid, msg_id, msg)
+    if Config.INCOMPLETE_TASK_NOTIFIER and Config.DATABASE_URL:
+        if notifier_dict := await database.get_incomplete_tasks():
+            for cid, data in notifier_dict.items():
+                msg = "Restarted Successfully!" if cid == chat_id else "Bot Restarted!"
+                for tag, links in data.items():
+                    msg += f"\n\n{tag}: "
+                    for index, link in enumerate(links, start=1):
+                        msg += f" <a href='{link}'>{index}</a> |"
+                        if len(msg.encode()) > 4000:
+                            await send_incomplete_task_message(cid, msg_id, msg)
+                            msg = ""
+                if msg:
+                    await send_incomplete_task_message(cid, msg_id, msg)
 
     if await aiopath.isfile(".restartmsg"):
-        with contextlib.suppress(Exception):
+        try:
             await TgClient.bot.edit_message_text(
-                chat_id=chat_id,
-                message_id=msg_id,
-                text="Restarted Successfully!",
+                chat_id=chat_id, message_id=msg_id, text="Restarted Successfully!"
             )
+        except:
+            pass
         await remove(".restartmsg")
 
 
@@ -179,7 +139,7 @@ async def confirm_restart(_, query):
             "pkill",
             "-9",
             "-f",
-            "gunicorn|xria|xnox|xtra|xone|xnzb|java|7z|split",
+            "gunicorn|aria2c|qbittorrent-nox|ffmpeg|rclone|java|sabnzbdplus|7z|split",
         )
         proc2 = await create_subprocess_exec("python3", "update.py")
         await gather(proc1.wait(), proc2.wait())
