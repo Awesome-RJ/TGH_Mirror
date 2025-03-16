d30eaff39f04cd00b9bcfc4a0172accb75a8c614
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-16 19:59:15 +0530

    Update __init__.py

--- tghbot/__init__.py
+++ tghbot/__init__.py
@@ -1,5 +1,4 @@
 # ruff: noqa: E402
-from uvloop import install
 
 install()
 
@@ -18,13 +17,14 @@ from logging import (
     basicConfig,
     getLogger,
 )
-from time import time
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pytz import timezone
 from uvloop import install
 
 from sabnzbdapi import SabnzbdClient
+from time import time
+from os import cpu_count
 
 getLogger("requests").setLevel(WARNING)
 getLogger("urllib3").setLevel(WARNING)
@@ -38,49 +38,20 @@ bot_start_time = time()
 bot_loop = new_event_loop()
 set_event_loop(bot_loop)
 
-
-class CustomFormatter(Formatter):
-    def formatTime(  # noqa: N802
-        self,
-        record: LogRecord,
-        datefmt: str | None,
-    ) -> str:
-        dt: datetime = datetime.fromtimestamp(
-            record.created,
-            tz=timezone("Asia/Dhaka"),
-        )
-        return dt.strftime(datefmt)
-
-    def format(self, record: LogRecord) -> str:
-        return super().format(record).replace(record.levelname, record.levelname[:1])
-
-
-formatter = CustomFormatter(
-    "[%(asctime)s] %(levelname)s - %(message)s [%(module)s:%(lineno)d]",
-    datefmt="%d-%b %I:%M:%S %p",
+basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[FileHandler("log.txt"), StreamHandler()],
+    level=INFO,
 )
 
-file_handler = FileHandler("log.txt")
-file_handler.setFormatter(formatter)
-
-stream_handler = StreamHandler()
-stream_handler.setFormatter(formatter)
-
-basicConfig(handlers=[file_handler, stream_handler], level=INFO)
-
 LOGGER = getLogger(__name__)
-
-cpu_no = os.cpu_count()
+cpu_no = cpu_count()
 
 DOWNLOAD_DIR = "/usr/src/app/downloads/"
-intervals = {
-    "status": {},
-    "qb": "",
-    "jd": "",
-    "nzb": "",
-    "stopAll": False,
-}
+intervals = {"status": {}, "qb": "", "jd": "", "nzb": "", "stopAll": False}
 qb_torrents = {}
+jd_downloads = {}
+nzb_jobs = {}
 user_data = {}
 aria2_options = {}
 qbit_options = {}
@@ -89,8 +60,6 @@ queued_dl = {}
 queued_up = {}
 status_dict = {}
 task_dict = {}
-jd_downloads = {}
-nzb_jobs = {}
 rss_dict = {}
 auth_chats = {}
 excluded_extensions = ["aria2", "!qB"]
@@ -104,35 +73,15 @@ multi_tags = set()
 task_dict_lock = Lock()
 queue_dict_lock = Lock()
 qb_listener_lock = Lock()
-cpu_eater_lock = Lock()
-same_directory_lock = Lock()
 nzb_listener_lock = Lock()
 jd_listener_lock = Lock()
-shorteners_list = []
+cpu_eater_lock = Lock()
+same_directory_lock = Lock()
 
 sabnzbd_client = SabnzbdClient(
     host="http://localhost",
     api_key="mltb",
     port="8070",
 )
-subprocess.run(["xnox", "-d", f"--profile={os.getcwd()}"], check=False)
-subprocess.run(
-    [
-        "xnzb",
-        "-f",
-        "sabnzbd/SABnzbd.ini",
-        "-s",
-        ":::8070",
-        "-b",
-        "0",
-        "-d",
-        "-c",
-        "-l",
-        "0",
-        "--console",
-    ],
-    check=False,
-)
-
 
 scheduler = AsyncIOScheduler(event_loop=bot_loop)
