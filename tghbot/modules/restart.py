4325fa946bc1b5451beb424cae3972a4718c926f
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-17 14:14:55 +0530

    Update restart.py

--- tghbot/modules/restart.py
+++ tghbot/modules/restart.py
@@ -15,34 +15,17 @@ from tghbot.core.torrent_manager import TorrentManager
 from tghbot.helper.ext_utils.bot_utils import new_task
 from tghbot.helper.ext_utils.db_handler import database
 from tghbot.helper.ext_utils.files_utils import clean_all
-from tghbot.helper.telegram_helper import button_build
-from tghbot.helper.telegram_helper.message_utils import delete_message, send_message
+from tghbot.helper.telegram_helper.message_utils import send_message, delete_message
 
 
 @new_task
 async def restart_bot(_, message):
-    buttons = button_build.ButtonMaker()
-    buttons.data_button("Yes!", "botrestart confirm")
-    buttons.data_button("Cancel", "botrestart cancel")
-    button = buttons.build_menu(2)
-    await send_message(
-        message,
-        "Are you sure you want to restart the bot?",
-        button,
-    )
+    await perform_restart(message)
 
 
 @new_task
 async def restart_sessions(_, message):
-    buttons = button_build.ButtonMaker()
-    buttons.data_button("Yes!", "sessionrestart confirm")
-    buttons.data_button("Cancel", "sessionrestart cancel")
-    button = buttons.build_menu(2)
-    await send_message(
-        message,
-        "Are you sure you want to restart the session(s)?",
-        button,
-    )
+    await perform_restart(message)
 
 
 async def send_incomplete_task_message(cid, msg_id, msg):
@@ -100,59 +83,50 @@ async def restart_notification():
 
 
 @new_task
-async def confirm_restart(_, query):
-    await query.answer()
-    data = query.data.split()
-    message = query.message
-    await delete_message(message)
-    if data[1] == "confirm":
-        reply_to = message.reply_to_message
-        intervals["stopAll"] = True
-        restart_message = await send_message(reply_to, "Restarting...")
-        await delete_message(message)
-        await TgClient.stop()
-        if scheduler.running:
-            scheduler.shutdown(wait=False)
-        if qb := intervals["qb"]:
-            qb.cancel()
-        if jd := intervals["jd"]:
-            jd.cancel()
-        if nzb := intervals["nzb"]:
-            nzb.cancel()
-        if st := intervals["status"]:
-            for intvl in list(st.values()):
-                intvl.cancel()
-        await clean_all()
-        await TorrentManager.close_all()
-        if sabnzbd_client.LOGGED_IN:
-            await gather(
-                sabnzbd_client.pause_all(),
-                sabnzbd_client.delete_job("all", True),
-                sabnzbd_client.purge_all(True),
-                sabnzbd_client.delete_history("all", delete_files=True),
-            )
-            await sabnzbd_client.close()
-        if jdownloader.is_connected:
-            await gather(
-                jdownloader.device.downloadcontroller.stop_downloads(),
-                jdownloader.device.linkgrabber.clear_list(),
-                jdownloader.device.downloads.cleanup(
-                    "DELETE_ALL",
-                    "REMOVE_LINKS_AND_DELETE_FILES",
-                    "ALL",
-                ),
-            )
-            await jdownloader.close()
-        proc1 = await create_subprocess_exec(
-            "pkill",
-            "-9",
-            "-f",
-            "gunicorn|aria2c|qbittorrent-nox|ffmpeg|rclone|java|sabnzbdplus|7z|split",
+async def perform_restart(message):
+    intervals["stopAll"] = True
+    restart_message = await send_message(message, "Restarting...")
+    await TgClient.stop()
+    if scheduler.running:
+        scheduler.shutdown(wait=False)
+    if qb := intervals["qb"]:
+        qb.cancel()
+    if jd := intervals["jd"]:
+        jd.cancel()
+    if nzb := intervals["nzb"]:
+        nzb.cancel()
+    if st := intervals["status"]:
+        for intvl in list(st.values()):
+            intvl.cancel()
+    await clean_all()
+    await TorrentManager.close_all()
+    if sabnzbd_client.LOGGED_IN:
+        await gather(
+            sabnzbd_client.pause_all(),
+            sabnzbd_client.delete_job("all", True),
+            sabnzbd_client.purge_all(True),
+            sabnzbd_client.delete_history("all", delete_files=True),
         )
-        proc2 = await create_subprocess_exec("python3", "update.py")
-        await gather(proc1.wait(), proc2.wait())
-        async with aiopen(".restartmsg", "w") as f:
-            await f.write(f"{restart_message.chat.id}\n{restart_message.id}\n")
-        osexecl(executable, executable, "-m", "bot")
-    else:
-        await delete_message(message)
+        await sabnzbd_client.close()
+    if jdownloader.is_connected:
+        await gather(
+            jdownloader.device.downloadcontroller.stop_downloads(),
+            jdownloader.device.linkgrabber.clear_list(),
+            jdownloader.device.downloads.cleanup(
+                "DELETE_ALL",
+                "REMOVE_LINKS_AND_DELETE_FILES",
+                "ALL",
+            ),
+        )
+        await jdownloader.close()
+    proc1 = await create_subprocess_exec(
+        "pkill",
+        "-9",
+        "-f",
+        "gunicorn|aria2c|qbittorrent-nox|ffmpeg|rclone|java|sabnzbdplus|7z|split",
+    )
+    proc2 = await create_subprocess_exec("python3", "update.py")
+    await gather(proc1.wait(), proc2.wait())
+    async with aiopen(".restartmsg", "w") as f:
+        await f.write(f"{restart_message.chat.id}\n{restart_message.id}\n")
+    osexecl(executable, executable, "-m", "bot")
