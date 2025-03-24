ee22451d32394258c47613b801dbb4442a911313
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-24 19:28:14 +0530

    Update restart.py

--- tghbot/modules/restart.py
+++ tghbot/modules/restart.py
@@ -1,8 +1,7 @@
 import os
 import sys
 
-from pyrogram import Client, filters
-
+from pyrogram.filters import command, regex
 from tghbot.helper.ext_utils.bot_utils import new_task
 from tghbot.helper.telegram_helper.message_utils import send_message
 
