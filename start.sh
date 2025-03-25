27ae04d25d716e9bcdf11a4b6c5e5b62373e8ac6
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-25 22:56:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 cd /root/TGH_Mirror/tghbot
 
 # Activate the virtual environment
-source /usr/local/bin/tgh-env
+source /usr/src/app/tgh-env
 
 # Install the requirements
-tgh-env pip install -r requirements.txt
+/usr/src/app/tgh-env pip install -r requirements.txt
 
 # Run the application
 python3 update.py
