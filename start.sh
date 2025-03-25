88c3463b4fdfe6ac5988819050a4f6f84d2a4d48
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-03-25 22:32:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ cd /root/TGH_Mirror/tghbot
 source tgh-env/bin/activate
 
 # Edit the requirements.txt file to comment out or remove problematic packages (if needed)
-nano ../requirements.txt
+nano requirements.txt
 
 # Install the requirements
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 
 # Run the application
 python3 update.py
