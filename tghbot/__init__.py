75d48fffb0075c4166a76fce011e0efd1d8e2235
Huntingbots2 <tgh191119@gmail.com>
2025-03-27 20:16:14 +0530

    Update __init__.py

--- tghbot/__init__.py
+++ tghbot/__init__.py
@@ -1,86 +1,62 @@
 # ruff: noqa: E402
+
 from uvloop import install
 
 install()
 
-import os
-import subprocess
+from subprocess import run as srun
+from os import getcwd
 from asyncio import Lock, new_event_loop, set_event_loop
-from datetime import datetime
 from logging import (
     ERROR,
     INFO,
     WARNING,
     FileHandler,
-    Formatter,
-    LogRecord,
     StreamHandler,
     basicConfig,
     getLogger,
 )
+from os import cpu_count
 from time import time
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from pytz import timezone
-from uvloop import install
+from pyrogram import utils as pyroutils
 
+from .core.config_manager import BinConfig
 from sabnzbdapi import SabnzbdClient
 
 getLogger("requests").setLevel(WARNING)
 getLogger("urllib3").setLevel(WARNING)
 getLogger("pyrogram").setLevel(ERROR)
+getLogger("aiohttp").setLevel(ERROR)
+getLogger("apscheduler").setLevel(ERROR)
 getLogger("httpx").setLevel(WARNING)
 getLogger("pymongo").setLevel(WARNING)
 getLogger("aiohttp").setLevel(WARNING)
 
+pyroutils.MIN_CHAT_ID = -999999999999
+pyroutils.MIN_CHANNEL_ID = -100999999999999
 bot_start_time = time()
 
 bot_loop = new_event_loop()
 set_event_loop(bot_loop)
 
-
-class CustomFormatter(Formatter):
-    def formatTime(  # noqa: N802
-        self,
-        record: LogRecord,
-        datefmt: str | None,
-    ) -> str:
-        dt: datetime = datetime.fromtimestamp(
-            record.created,
-            tz=timezone("Asia/Dhaka"),
-        )
-        return dt.strftime(datefmt)
-
-    def format(self, record: LogRecord) -> str:
-        return super().format(record).replace(record.levelname, record.levelname[:1])
-
-
-formatter = CustomFormatter(
-    "[%(asctime)s] %(levelname)s - %(message)s [%(module)s:%(lineno)d]",
-    datefmt="%d-%b %I:%M:%S %p",
+basicConfig(
+    format="[%(asctime)s] [%(levelname)s] - %(message)s",  #  [%(filename)s:%(lineno)d]
+    datefmt="%d-%b-%y %I:%M:%S %p",
+    handlers=[FileHandler("log.txt"), StreamHandler()],
+    level=INFO,
 )
 
-file_handler = FileHandler("log.txt")
-file_handler.setFormatter(formatter)
-
-stream_handler = StreamHandler()
-stream_handler.setFormatter(formatter)
-
-basicConfig(handlers=[file_handler, stream_handler], level=INFO)
-
 LOGGER = getLogger(__name__)
+cpu_no = cpu_count()
 
-cpu_no = os.cpu_count()
-
+bot_cache = {}
 DOWNLOAD_DIR = "/usr/src/app/downloads/"
-intervals = {
-    "status": {},
-    "qb": "",
-    "jd": "",
-    "nzb": "",
-    "stopAll": False,
-}
+intervals = {"status": {}, "qb": "", "jd": "", "nzb": "", "stopAll": False}
 qb_torrents = {}
+jd_downloads = {}
+nzb_jobs = {}
 user_data = {}
 aria2_options = {}
 qbit_options = {}
@@ -89,9 +65,19 @@ queued_dl = {}
 queued_up = {}
 status_dict = {}
 task_dict = {}
-jd_downloads = {}
-nzb_jobs = {}
 rss_dict = {}
+shortener_dict = {}
+var_list = [
+    "BOT_TOKEN",
+    "TELEGRAM_API",
+    "TELEGRAM_HASH",
+    "OWNER_ID",
+    "DATABASE_URL",
+    "BASE_URL",
+    "UPSTREAM_REPO",
+    "UPSTREAM_BRANCH",
+    "UPDATE_PKGS",
+]
 auth_chats = {}
 excluded_extensions = ["aria2", "!qB"]
 drives_names = []
@@ -104,35 +90,17 @@ multi_tags = set()
 task_dict_lock = Lock()
 queue_dict_lock = Lock()
 qb_listener_lock = Lock()
-cpu_eater_lock = Lock()
-same_directory_lock = Lock()
 nzb_listener_lock = Lock()
 jd_listener_lock = Lock()
+cpu_eater_lock = Lock()
+same_directory_lock = Lock()
 shorteners_list = []
 
 sabnzbd_client = SabnzbdClient(
     host="http://localhost",
-    api_key="mltb",
+    api_key="admin",
     port="8070",
 )
-subprocess.run(["xnox", "-d", f"--profile={os.getcwd()}"], check=False)
-subprocess.run(
-    [
-        "xnzb",
-        "-f",
-        "sabnzbd/SABnzbd.ini",
-        "-s",
-        ":::8070",
-        "-b",
-        "0",
-        "-d",
-        "-c",
-        "-l",
-        "0",
-        "--console",
-    ],
-    check=False,
-)
-
+srun([BinConfig.QBIT_NAME, "-d", f"--profile={getcwd()}"], check=False)
 
 scheduler = AsyncIOScheduler(event_loop=bot_loop)
