cf86e85ab726eff98117f5002a32ba74d79d8634
Huntingbots2 <tgh191119@gmail.com>
2025-03-28 13:33:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-python3 update.py && python3 -m tghbot
+#!/bin/bash
+
+# Start Aria2 in daemon mode
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --daemon=true
+
+# Your existing start commands
+# ...
+
+# Start your bot application
+python3 -m tghbot
