67bbe195635c5333e7dae39712f99337992621c7
Huntingbots2 <tgh191119@gmail.com>
2025-04-03 13:30:52 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-DATABASE_PATH="postgresql://tghbot_owner:npg_jae9mlh4kOMN@ep-shy-feather-a4hofch2-pooler.us-east-1.aws.neon.tech/tghbot?sslmode=require"  # Update with your actual database file path
+DATABASE_URL="postgresql://tghbot_owner:npg_jae9mlh4kOMN@ep-shy-feather-a4hofch2-pooler.us-east-1.aws.neon.tech/tghbot?sslmode=require"  # Update with your actual database URL
 
-# Check if the database file exists
-if [ ! -f "$DATABASE_PATH" ]; then
-    echo "Error: Database file does not exist at $DATABASE_PATH"
+# Ensure the database URL is set
+if [ -z "$DATABASE_URL" ]; then
+    echo "Error: Database URL is not set"
     exit 1
 fi
-
-# Ensure the file has correct permissions
-chmod 664 "$DATABASE_PATH"
 
 # Execute the main command
 exec "$@"
