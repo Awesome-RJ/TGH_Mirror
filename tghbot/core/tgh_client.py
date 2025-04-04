305a7ddd32b7b9b84cdc3a3510eb30f34c8450dd
Huntingbots2 <tgh191119@gmail.com>
2025-04-04 21:34:01 +0530

    Update tgh_client.py

--- tghbot/core/tgh_client.py
+++ tghbot/core/tgh_client.py
@@ -28,7 +28,6 @@ class TgClient:
             bot_token=Config.BOT_TOKEN,
             workdir="/usr/src/app",
             parse_mode=enums.ParseMode.HTML,
-            max_concurrent_transmissions=10,
         )
         await cls.bot.start()
         cls.NAME = cls.bot.me.username
@@ -46,7 +45,6 @@ class TgClient:
                     session_string=Config.USER_SESSION_STRING,
                     parse_mode=enums.ParseMode.HTML,
                     no_updates=True,
-                    max_concurrent_transmissions=10,
                 )
                 await cls.user.start()
                 cls.IS_PREMIUM_USER = cls.user.me.is_premium
