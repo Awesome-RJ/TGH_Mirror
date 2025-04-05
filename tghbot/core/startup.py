01d622f46409154bc08537a92e4f429d47a34496
Huntingbots2 <tgh191119@gmail.com>
2025-04-05 06:53:08 +0530

    Update startup.py

--- tghbot/core/startup.py
+++ tghbot/core/startup.py
@@ -39,9 +39,9 @@ async def update_qb_options():
         for k in list(qbit_options.keys()):
             if k.startswith("rss"):
                 del qbit_options[k]
-        qbit_options["web_ui_password"] = "mltbmltb"
+        qbit_options["web_ui_password"] = "sabpassword"
         await TorrentManager.qbittorrent.app.set_preferences(
-            {"web_ui_password": "mltbmltb"},
+            {"web_ui_password": "sabpassword"},
         )
     else:
         await TorrentManager.qbittorrent.app.set_preferences(qbit_options)
