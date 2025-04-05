eb661b3cd9a0b5107d277a66c3e1a714fadb89dc
HuntingBots <huntingbots.tg@gmail.com>
2025-04-05 00:05:29 +0000

    format: Auto-format code by ruff [skip actions].

--- tghbot/core/startup.py
+++ tghbot/core/startup.py
@@ -1,6 +1,8 @@
+import asyncio
 from asyncio import create_subprocess_exec, create_subprocess_shell
 from os import environ
 
+import httpx
 from aiofiles import open as aiopen
 from aiofiles.os import makedirs, remove
 from aiofiles.os import path as aiopath
@@ -26,8 +28,6 @@ from tghbot.core.config_manager import Config
 from tghbot.core.tgh_client import TgClient
 from tghbot.core.torrent_manager import TorrentManager
 from tghbot.helper.ext_utils.db_handler import database
-import asyncio
-import httpx
 
 
 async def update_qb_options():
@@ -69,7 +69,7 @@ async def update_nzb_options():
         except (httpx.ConnectError, httpx.RequestError) as e:
             LOGGER.error(f"Connection attempt {attempt + 1} failed: {e}")
             if attempt < retries - 1:
-                await asyncio.sleep(2 ** attempt)
+                await asyncio.sleep(2**attempt)
             else:
                 raise e
 
