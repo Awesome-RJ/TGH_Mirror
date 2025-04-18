c8a347fbba95edae228626a2b5333246980899f6
Huntingbots2 <tgh191119@gmail.com>
2025-04-18 16:13:40 +0530

    Update config.py
    Added access  as BOT TOKEN

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # REQUIRED CONFIG
-BOT_TOKEN = "7713486921:AAF4akLKvq-XUZnJg6DBy-nFeRn0GHwa3Hs" # Enter your telegram bot token
+BOT_TOKEN = "8061290582:AAGyQYjJtvlI4FVn_iwEfHborIwz_g4jzqw" # Enter your telegram bot token
 OWNER_ID = 6200648859 # Enter your telegram user id
 TELEGRAM_API = 3975570 # Enter telegram API key
 TELEGRAM_HASH = "680b62f2844aa1954216f6cb99d2f3d9" # Enter telegram API hash
