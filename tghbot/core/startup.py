fa69f98b1cafcbcc92e510c764722d0aa11aa75f
Huntingbots2 <tgh191119@gmail.com>
2025-05-15 22:05:24 +0530

    Update startup.py
    Bug fixed

--- tghbot/core/startup.py
+++ tghbot/core/startup.py
@@ -1,4 +1,6 @@
 import asyncio
+from asyncio import create_subprocess_exec, create_subprocess_shell
+from os import environ
 
 import httpx
 from aiofiles import open as aiopen
@@ -10,12 +12,20 @@ from sabnzbdapi import SabnzbdClient
 from tghbot import (
     LOGGER,
     aria2_options,
+    auth_chats,
+    drives_ids,
+    drives_names,
+    excluded_extensions,
+    index_urls,
     nzb_options,
     qbit_options,
     rss_dict,
+    shorteners_list,
+    sudo_users,
     user_data,
 )
 from tghbot.core.config_manager import Config
+from tghbot.core.tgh_client import TgClient
 from tghbot.core.torrent_manager import TorrentManager
 from tghbot.helper.ext_utils.db_handler import database
 
@@ -47,15 +57,13 @@ async def update_aria2_options():
 async def update_nzb_options():
     sabnzbd_client_instance = SabnzbdClient(
         host="http://localhost",  # Update this to the correct host
-        api_key="mltb",  # Ensure this API key is correct
-        port="8070",  # Verify the port
+        api_key="mltb",          # Ensure this API key is correct
+        port="8070",             # Verify the port
     )
     retries = 3
     for attempt in range(retries):
         try:
-            LOGGER.info(
-                f"Attempting to connect to Sabnzbd (Attempt {attempt + 1}/{retries})...",
-            )
+            LOGGER.info(f"Attempting to connect to Sabnzbd (Attempt {attempt + 1}/{retries})...")
             no = (await sabnzbd_client_instance.get_config())["config"]["misc"]
             nzb_options.update(no)
             LOGGER.info(
@@ -68,9 +76,7 @@ async def update_nzb_options():
                 LOGGER.info("Retrying connection after delay...")
                 await asyncio.sleep(2**attempt)
             else:
-                LOGGER.error(
-                    "All connection attempts to Sabnzbd failed. Please check the service status and configuration.",
-                )
+                LOGGER.error("All connection attempts to Sabnzbd failed. Please check the service status and configuration.")
                 raise e
 
 
